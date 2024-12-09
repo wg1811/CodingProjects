5f5a19d573bd3c70007c78654e9d54a7cd9d9df6
Wesley G <wg1811.kodehode@gmail.com>
2024-12-09 11:32:24 +0100

    Update .gitignore and remove unnecessary files

---
+++ C#/learningClasses/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+namespace learningClasses
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Person person = new Person()
+            {
+                Name = "Ted",
+                Age = 24,
+                City = "London",
+                HasPet = true,
+            };
+
+            person.PersonInfo();
+
+            Car newCar = new Car()
+            {
+                Make = "Porsche",
+                Model = "911",
+                Year = 1970,
+                Color = "Yellow",
+                FourWheelDrive = false,
+            };
+
+            newCar.ShowCarInfo();
+
+        }
+    }
+}
