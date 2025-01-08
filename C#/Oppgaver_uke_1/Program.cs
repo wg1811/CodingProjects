31daf354831186b4d41c3fc0494df4d33d41d16a
Wesley <wg1811.kodehode@gmail.com>
2025-01-08 10:34:26 +0100

    Adding C# Homework

---
+++ C#/Oppgaver_uke_1/Program.cs
@@ -0,0 +1,39 @@
+﻿namespace Oppgaver_uke_1;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+    
+    int arbInt = 7;
+    string arbString = "Sandakerveien";
+    double arbDouble = 114.01;
+    char arbChar = 'k';
+    int[] arbIntArray = [1, 23, 43, 4, 6, 7, 4, 85, 3];
+    
+    for(int i = 0; i > arbIntArray.Length; i++)
+    {
+        Console.WriteLine(arbIntArray[i]);
+    }
+
+    string[] arbStringArray = ["Cat", "Hat", "Bat", "Sat", "Mat", "Nat", "Rat"];
+    foreach (string word in arbStringArray)
+    {
+        Console.WriteLine(word);
+    }
+
+    List<string> arbListString = new List<string> {"Joe", "Joakim", "Sigrid"};
+    arbListString.Add("Javier");
+    arbListString.Add("Jermain");
+    arbListString.Add("Kim");
+    
+    foreach (string word in arbListString)
+    {
+        Console.WriteLine(word);
+    }
+    
+    }
+
+    
+}
