88abd9641ef02da5df67622f51965ba38f9bb513
Wesley <wesley.t.george@gmail.com>
2025-01-18 15:20:39 +0100

    Weather Sim classes and starting program.cs

--- Cases/weatherSim/WeatherSim/Weather.cs
+++ Cases/weatherSim/WeatherSim/Weather.cs
@@ -4,71 +4,90 @@
     class Weather
     {
         public double Temp { get; set; } = 0;
-        public string Precipitation { get; set;} = "";
+        public string Precipitation { get; set; } = "";
         public double Cloudy { get; set; } = 0;
         public string Deadly { get; set; } = "none";
 
         // Declare the Random instance
         private Random random = new Random();
-    
+
         // Constructor
-        public Weather()
+        public Weather() { }
+
+        public int getTemp()
         {
-
-        }
-
-        public int getTemp() {
             return random.Next(-40, 50);
         }
 
-        public string getPrecipitation() {
-            string[] precipitationTypes = ["rain", "sleet", "snow", "hail", "fog", "drizzle", "heavy rain"];
+        public string getPrecipitation()
+        {
+            string[] precipitationTypes =
+            [
+                "rain",
+                "sleet",
+                "snow",
+                "hail",
+                "fog",
+                "drizzle",
+                "heavy rain",
+            ];
             int precType = random.Next(0, precipitationTypes.Length - 1);
             this.Precipitation = precipitationTypes[precType];
             int chancePrep = random.Next(0, 101);
-            if (this.Temp < -1 && chancePrep > 40) {
+            if (this.Temp < -1 && chancePrep > 40)
+            {
                 this.Precipitation = "snow";
                 return this.Precipitation;
-                } else if (chancePrep > 40) {
-                    return this.Precipitation;
-                    } else return "sunny";
+            }
+            else if (chancePrep > 40)
+            {
+                return this.Precipitation;
+            }
+            else
+                return "sunny";
         }
 
-        public string getDeadlyWeather() {
+        public string getDeadlyWeather()
+        {
             string[] deadlyWeather = ["tornado", "hurricane", "wild fires", "tsunami"];
             int badWeatherChance = random.Next(0, 101);
-            int badWeatherType = random.Next(0,4);
-            if (badWeatherChance < 6) {
+            int badWeatherType = random.Next(0, 4);
+            if (badWeatherChance < 6)
+            {
                 this.Deadly = deadlyWeather[badWeatherType];
             }
             return this.Deadly;
         }
 
-        public int getCloudy() {
+        public int getCloudy()
+        {
             int chanceCloudy = random.Next(0, 101);
-            if (chanceCloudy > 21) {
+            if (chanceCloudy > 21)
+            {
                 return 0;
-            } else return random.Next(21, 101);
+            }
+            else
+                return random.Next(21, 101);
         }
 
-        public void generateWeather() {
+        public void generateWeather()
+        {
             this.Temp = getTemp();
             this.Precipitation = getPrecipitation();
             this.Cloudy = getCloudy();
             string reallyBadWeather = getDeadlyWeather();
 
             // Display weather
-            Console.WriteLine($@"
+            Console.WriteLine(
+                $@"
                 ____________________________
                 Today's Weather:
                 Temperature: {this.Temp}
                 Conditions: {this.Precipitation}
                 Cloudiness: {this.Cloudy}%
                 Extreme Weather: {reallyBadWeather}
-                ___________________________");
+                ___________________________"
+            );
         }
     }
-
 }
-
-
