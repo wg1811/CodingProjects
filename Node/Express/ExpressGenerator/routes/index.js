b4e4255861bd2a7391e8d8543b1e48246c2b94d0
Wesley <wg1811.kodehode@gmail.com>
2025-02-14 10:26:45 +0100

    Express Generator start example.

--- Node/Express/ExpressGenerator/routes/index.js
+++ Node/Express/ExpressGenerator/routes/index.js
@@ -1,26 +1,39 @@
 var express = require('express');
-const { path } = require('../app'); // I'm getting path errors...need to install path? or maybe it should be var path = something?
+var path = require('path'); // I'm getting path errors...need to install path? or maybe it should be var path = something?
 var router = express.Router();
 var fs = require("fs");
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
+// /* GET home page. */
+// router.get('/', function(req, res, next) {
+//   res.render('index', { title: 'Express' });
+// });
 
 //  Read people.json and render it
-router.get('/people', function(req, res, next) {
+router.get('/', function(req, res, next) {
   // how to add the people file
+  console.log("It got this far");
   const filePath = path.join(__dirname, "../public/data/people.json");  // maybe this should be "public", "data", etc.?
-  fs.readFile, "utf-8", (err,data) => {
+  console.log(filePath + " is the file path.");
+
+  fs.readFile(filePath, "utf-8", (err, data) => {
     if (err) {
-      console.error("error", err)
-      return res.status(500).send("Error");
+      console.error("Error reading file:", err);
+      if (!res.headersSent) res.status(500).send("Error reading data file"); 
+      return;
     }
-    const people = JSON.parse(data);
-    console.log(people);
-    res.render('index', { title: 'Express', people:JSON.stringify(people) });
-  }
+
+    let people;
+    try {
+      people = JSON.parse(data); // Ensure it's parsed properly
+    } catch (parseError) {
+      console.error("Error parsing JSON:", parseError);
+      if (!res.headersSent) res.status(500).send("Error reading data file"); 
+      return;
+    }
+
+    console.log("Loaded people:", people); // Debugging output
+    res.render('index', { title: 'Express', people }); // ✅ Pass people as an array
+  });
 });
 
 module.exports = router;
